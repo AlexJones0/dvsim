de0a54a83c3b066e9f5f9d1e9a593b559736d1a2
Alex Jones <alex.jones@lowrisc.org>
2026-03-27 05:27:36 +0000

    feat: switch to use the async scheduler interface
    This commit makes the full transition to use the new async scheduler in
    place of the old scheduler, instead of hiding it behind an experimental
    environment variable.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- src/dvsim/flow/base.py
+++ src/dvsim/flow/base.py
@@ -20,13 +20,11 @@ from dvsim import instrumentation
 from dvsim.flow.hjson import set_target_attribute
 from dvsim.job.data import CompletedJobStatus, JobSpec
 from dvsim.job.status import JobStatus
-from dvsim.launcher.factory import get_launcher_cls
 from dvsim.logging import log
 from dvsim.runtime.fake import FakeRuntimeBackend
 from dvsim.runtime.registry import backend_registry
-from dvsim.scheduler.async_core import Scheduler as AsyncScheduler
+from dvsim.scheduler.async_core import Scheduler
 from dvsim.scheduler.async_status_printer import create_status_printer
-from dvsim.scheduler.core import Scheduler
 from dvsim.scheduler.log_manager import LogManager
 from dvsim.utils import (
     find_and_substitute_wildcards,
@@ -456,14 +454,7 @@ class FlowCfg(ABC):
                 ),
             )
 
-        if self.args.experimental_enable_async_scheduler:
-            return asyncio.run(self.run_scheduler(jobs))
-
-        return Scheduler(
-            items=jobs,
-            launcher_cls=get_launcher_cls(),
-            interactive=self.interactive,
-        ).run()
+        return asyncio.run(self.run_scheduler(jobs))
 
     async def run_scheduler(self, jobs: list[JobSpec]) -> list[CompletedJobStatus]:
         """Run the scheduler with the given set of job specifications."""
@@ -475,7 +466,7 @@ class FlowCfg(ABC):
         if isinstance(default_backend, FakeRuntimeBackend):
             default_backend.attach_fake_policy(lambda job: self._fake_policy(jobs, job))
 
-        scheduler = AsyncScheduler(
+        scheduler = Scheduler(
             jobs=jobs,
             backends={default_backend.name: default_backend},
             default_backend=default_backend.name,
