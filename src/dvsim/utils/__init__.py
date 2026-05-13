2312c5978a8355ede203de5a639c3f19c9ca9124
Alex Jones <alex.jones@lowrisc.org>
2026-05-13 11:48:44 +0100

    feat: add utility function for getting the suffix of an ordinal
    Nothing exists to do this in the standard library or in our existing
    dependencies; other dependencies e.g. `pandas` could be added to do this
    but given that the functionality is simple enough, just implement it
    ourselves as a basic utility function.
    
    This will be used to help format positional/ranking information in
    instrumentation report visualisations, e.g. showing that a job is the
    Xth longest job.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- src/dvsim/utils/__init__.py
+++ src/dvsim/utils/__init__.py
@@ -7,6 +7,7 @@
 from dvsim.utils.check import check_bool, check_int
 from dvsim.utils.fs import clean_odirs, mk_path, mk_symlink, rm_path
 from dvsim.utils.hjson import parse_hjson
+from dvsim.utils.print import ordinal_suffix, print_msg_list
 from dvsim.utils.subprocess import run_cmd, run_cmd_with_timeout
 from dvsim.utils.time import TS_FORMAT, TS_FORMAT_LONG, TS_HMS_FORMAT
 from dvsim.utils.wildcards import (
@@ -24,7 +25,9 @@ __all__ = (
     "find_and_substitute_wildcards",
     "mk_path",
     "mk_symlink",
+    "ordinal_suffix",
     "parse_hjson",
+    "print_msg_list",
     "rm_path",
     "run_cmd",
     "run_cmd_with_timeout",
